36855fa9ada5f5fcc9ef4d4cdf3daf3e81bada74
Xalsar <xavicarreragimbert@gmail.com>
2020-11-07 14:40:10 +0100

    Create Thumbnail component

---
+++ components/Thumbnail/Thumbnail.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View } from "react-native";
+
+const AlbumList = (props) => {
+  return <View style={styles.item}></View>;
+};
+
+const styles = {
+  item: {
+    width: 85,
+    height: 85,
+    backgroundColor: "lightblue",
+    borderRadius: 10,
+  },
+};
+
+export default AlbumList;
