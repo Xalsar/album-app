588a1012b0e8122e439cca66729d47529f41c8d6
Xalsar <xavicarreragimbert@gmail.com>
2020-11-07 15:53:50 +0100

    Create Photo container

---
+++ containers/Photo/Photo.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+
+const Photo = ({ route }) => {
+  const { title, url } = route.params;
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          width: 300,
+          height: 300,
+          uri: url,
+        }}
+        fadeDuration={1000}
+      />
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    marginTop: 10,
+    fontSize: 20,
+    textAlign: "center",
+    marginTop: 30,
+  },
+};
+
+export default Photo;
