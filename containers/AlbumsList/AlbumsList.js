4951f5cfa8e71f350074f1134d1d1909d27174b8
Xalsar <xavicarreragimbert@gmail.com>
2020-11-07 14:45:42 +0100

    Set links to Albums AlbumsList

--- containers/AlbumsList/AlbumsList.js
+++ containers/AlbumsList/AlbumsList.js
@@ -3,32 +3,38 @@ import axios from "axios";
 import Album from "../../components/Album/Album";
 import { ScrollView, Text, View, TouchableOpacity } from "react-native";
 
-const AlbumList = (props) => {
+const AlbumList = ({ navigation }) => {
   const [albums, setAlbums] = useState([]);
+
   useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/albums?userId=1")
-      .then((response) => {
-        setAlbums(response.data);
+      .then(({ data }) => {
+        setAlbums(data);
       });
   }, []);
 
   return (
-    <ScrollView
-      showsVerticalScrollIndicator={false}
-      style={{
-        flex: 1,
-        flexDirection: "column",
-        backgroundColor: "#fff !important",
-      }}
-    >
+    <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
       {albums.map((album, id) => (
-        <Album key={id} title={album.title} />
+        <Album
+          key={id}
+          title={album.title}
+          handlePress={() => {
+            navigation.navigate("Album", { id: 1, title: album.title });
+          }}
+        />
       ))}
     </ScrollView>
   );
 };
 
-const styles = {};
+const styles = {
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#fff !important",
+  },
+};
 
 export default AlbumList;
