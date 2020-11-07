94781ef3d5014fe70964befda8e5525875817598
Xalsar <xavicarreragimbert@gmail.com>
2020-11-07 12:15:19 +0100

    Add navigation option to App

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import AlbumList from "./containers/AlbumsList/AlbumsList";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <AlbumList />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Home" component={AlbumList} />
+        </Stack.Navigator>
+      </View>
+    </NavigationContainer>
   );
 }
 
