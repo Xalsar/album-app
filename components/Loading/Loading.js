e383c01e149da8108c36e6c88bbc7b92c71b2fc2
Xalsar <xavicarreragimbert@gmail.com>
2020-11-07 17:34:32 +0100

    Add Loading component

---
+++ components/Loading/Loading.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, ActivityIndicator } from "react-native";
+
+const Loading = () => (
+  <View style={styles.loadingContainer}>
+    <ActivityIndicator size="large" color="lightblue" />
+  </View>
+);
+
+const styles = {
+  loadingContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+};
+
+export default Loading;
