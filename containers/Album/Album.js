e3679cde10022e6eb28d36957ff3318a32bf6f87
Xalsar <xavicarreragimbert@gmail.com>
2020-11-07 14:40:40 +0100

    Create Album container

---
+++ containers/Album/Album.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { ScrollView, Text } from "react-native";
+import PhotoItem from "../../components/PhotoItem/PhotoItem";
+
+const Album = ({ route }) => {
+  const { id, title } = route.params;
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/photos?albumId=${id}`)
+      .then(({ data }) => {
+        setPhotos(data);
+      });
+  }, []);
+
+  const capitalized = title.charAt(0).toUpperCase() + title.slice(1);
+
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <Text style={styles.title}>{capitalized}</Text>
+      {photos.map((photo, id) => (
+        <PhotoItem key={id}>{photo.title}</PhotoItem>
+      ))}
+    </ScrollView>
+  );
+};
+
+const styles = {
+  title: {
+    fontSize: 22,
+    paddingTop: 10,
+  },
+};
+
+export default Album;
