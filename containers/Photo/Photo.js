657b71e4f29eeac04ff67429857c8b06d86526fa
Xalsar <xavicarreragimbert@gmail.com>
2020-11-09 21:48:03 +0100

    Add pich-to-zoom into Photo

--- containers/Photo/Photo.js
+++ containers/Photo/Photo.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Animated, Dimensions } from "react-native";
+import { PinchGestureHandler, State } from "react-native-gesture-handler";
 import Loading from "../../components/Loading/Loading";
 import Error from "../../components/Error/Error";
 import Header from "../../components/Header/Header";
 import capitalize from "../../utilities/capitalize";
+
+const screen = Dimensions.get("window");
 
 const Photo = ({ route, navigation }) => {
   const { id } = route.params;
@@ -24,24 +27,48 @@ const Photo = ({ route, navigation }) => {
     return <Error />;
   }
 
-  return (
-    <View flex={1}>
-      <Header title={"Photo"} navigation={navigation} />
-      <View style={styles.container}>
-        <Image
-          source={{
-            width: 300,
-            height: 300,
-            uri: photo.url,
-          }}
-          fadeDuration={1000}
-        />
-        <Text style={styles.title}>{capitalize(photo.title)}</Text>
-      </View>
-    </View>
+  const scale = new Animated.Value(1);
+
+  const onPinchEvent = Animated.event(
+    [
+      {
+        nativeEvent: { scale: scale },
+      },
+    ],
+    {
+      useNativeDriver: true,
+    }
   );
 
-  return content;
+  const onPinchStateChange = (event) => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      Animated.spring(scale, {
+        toValue: 1,
+        useNativeDriver: true,
+      }).start();
+    }
+  };
+
+  return (
+    <View>
+      <Header title={"Photo"} navigation={navigation} />
+      <PinchGestureHandler
+        onGestureEvent={onPinchEvent}
+        onHandlerStateChange={onPinchStateChange}
+      >
+        <Animated.Image
+          source={{ uri: photo.url }}
+          style={{
+            width: screen.width,
+            height: 300,
+            transform: [{ scale: scale }],
+          }}
+          resizeMode="contain"
+        />
+      </PinchGestureHandler>
+      <Text style={styles.title}>{capitalize(photo.title)}</Text>
+    </View>
+  );
 };
 
 const styles = {
@@ -57,6 +84,7 @@ const styles = {
     marginTop: 30,
     fontFamily: "Roboto_400Regular",
     color: "#2c272d",
+    zIndex: -2,
   },
 };
 
